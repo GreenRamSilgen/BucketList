fd83dbb59c02f5f9ff33f69d1b28a30ab4c35e87
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-22 07:42:54 -0800

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Kiran Shrestha on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
